a805cd59cf7432f65bfd3639cc3d5b2484e9c71d
Mark Okoh <markokoh@yahoo.co.uk>
2023-01-09 23:15:29 -0800

    Added candidate images to project.

--- src/components/Vote.vue
+++ src/components/Vote.vue
@@ -1,22 +1,30 @@
 <!-- @format -->
 
 <template>
-	<h1>Live Text Vote <button class="reset" @click="resetVote">reset vote</button></h1>
+	<h1 class="align-center">Live Text Vote <button class="reset" @click="resetVote">reset vote</button></h1>
 	<h2>Vote Line: +1 (670) 366-2467</h2>
 	<div>Watch for live update when your vote is cast.</div>
 	<div>
-		<div :style="{ width: markWidth }" class="voteLevel background-blue">
-			Mark ({{ parseFloat(markWidth) / 70 }})
+		<div class="align-end">
+			<div :style="{ width: markWidth }" class="voteLevel background-blue">
+				Mark ({{ parseFloat(markWidth) / 70 }})
+			</div>
+			<img src="../assets/images/Guy.png" :width="markImageWidth" alt="" />
 		</div>
 		<h3>Text "1" to vote for Mark</h3>
-
-		<div :style="{ width: johnWidth }" class="voteLevel background-green">
-			John ({{ parseFloat(johnWidth) / 70 }})
+		<div class="align-end">
+			<div :style="{ width: johnWidth }" class="voteLevel background-green">
+				John ({{ parseFloat(johnWidth) / 70 }})
+			</div>
+			<img src="../assets/images/AfroFem.png" :width="johnImageWidth" alt="" />
 		</div>
 		<h3>Text "2" to vote for John</h3>
 
-		<div :style="{ width: steveWidth }" class="voteLevel background-red">
-			Steve ({{ parseFloat(steveWidth) / 70 }})
+		<div class="align-end">
+			<div :style="{ width: steveWidth }" class="voteLevel background-red">
+				Steve ({{ parseFloat(steveWidth) / 70 }})
+			</div>
+			<img src="../assets/images/IndianMan.png" :width="steveImageWidth" alt="" />
 		</div>
 		<h3>Text "3" to vote for Steve</h3>
 	</div>
@@ -27,13 +35,17 @@
 	import { db } from "../firebase/config";
 	import { setDoc, doc, onSnapshot } from "firebase/firestore";
 
-	const increment = (votecount) => {
-		// 1 VOTE IS REPRESENTED BY 70px
-		return votecount * 70;
+	const increment = (votecount, num) => {
+		return votecount * num;
 	};
+
 	const markWidth = ref("35px");
 	const johnWidth = ref("35px");
 	const steveWidth = ref("35px");
+
+	const markImageWidth = ref(150);
+	const johnImageWidth = ref(150);
+	const steveImageWidth = ref(150);
 
 	const resetVote = async () => {
 		await setDoc(doc(db, "elections", "exampleVote"), {
@@ -45,9 +57,13 @@
 
 	onSnapshot(doc(db, "elections", "exampleVote"), (doc) => {
 		const { Mark, John, Steve } = doc.data();
-		markWidth.value = increment(Mark) + "px";
-		johnWidth.value = increment(John) + "px";
-		steveWidth.value = increment(Steve) + "px";
+		markWidth.value = increment(Mark, 70) + "px";
+		johnWidth.value = increment(John, 70) + "px";
+		steveWidth.value = increment(Steve, 70) + "px";
+
+		markImageWidth.value = 150 + increment(Mark, 8);
+		johnImageWidth.value = 150 + increment(John, 8);
+		steveImageWidth.value = 150 + increment(John, 8);
 	});
 </script>
 
@@ -57,7 +73,7 @@
 		height: 35px;
 		min-width: 35px;
 		font-size: large;
-		margin: 35px 0px 20px 0px;
+		margin: 35px 0px 0px 0px;
 		padding: 20px;
 		font-weight: bold;
 		display: flex;
@@ -74,8 +90,14 @@
 		background-color: transparent;
 	}
 
-	h1 {
+	.align-center {
 		display: flex;
 		align-items: center;
+	}
+
+	.align-end {
+		display: flex;
+		align-items: end;
+		height: 175px;
 	}
 </style>
